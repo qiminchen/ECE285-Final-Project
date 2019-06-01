5397d3ecbbd58f9224f2462e00971e00fc89f066
qiminchen <liam951120@gmail.com>
2019-05-31 23:53:09 -0700

    Update utilis.py
    modify utilis.py

--- Neural Style Transfer/utilis.py
+++ Neural Style Transfer/utilis.py
@@ -20,57 +20,41 @@ import copy
 import pandas as pd
 import os
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Dataset
-class StyleTransferDataset(Dataset):
-    def __init__(self, root_dir, img_type, image_size=(512, 512)):
-        super(StyleTransferDataset, self).__init__()
-        self.image_size = image_size
-        self.images_dir = os.path.join(root_dir, img_type)
-    
-    
-    def __getitem__(self, types, img_name):
-        
-        img_path = os.path.join(self.images_dir, types, img_name)
-        img = Image.open(img_path).convert('RGB')
-        transform = transforms.Compose([
-            transforms.Resize(self.image_size),
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225]),
-        ])
-        
-        img = transform(img)
-    
-        return img
-    
+imsize = 512
+
+loader = transforms.Compose([
+    transforms.Resize((imsize,imsize)),
+    transforms.ToTensor()])
 
 
-# Content Loss
+def image_loader(image_name):
+    
+    image = Image.open(image_name)
+    image = loader(image).unsqueeze(0)
+    
+    return image.to(device, torch.float)
+
+
 class ContentLoss(nn.Module):
-    
-    def __init__(self, target):
+
+    def __init__(self, target,):
         super(ContentLoss, self).__init__()
-        # 'detach' the target content from the tree used
-        # to dynamically compute the gradient
         self.target = target.detach()
-        
+
     def forward(self, input):
         
         self.loss = F.mse_loss(input, self.target)
         
         return input
     
-# Style Loss
 def gram_matrix(input):
     
-    # a: batch size
-    # b: number of feature maps
-    # (c,d): dimensions of a feature map
-    
-    a, b, c, d = input.size()
+    a, b, c, d = input.size()  
     features = input.view(a * b, c * d)
     G = torch.mm(features, features.t())
-    
+
     # Normalize the values of the gram matrix
     return G.div(a * b * c * d)
 
@@ -86,41 +70,43 @@ class StyleLoss(nn.Module):
         self.loss = F.mse_loss(G, self.target)
         
         return input
-    
-    
-# Model
-vgg19 = models.vgg19(pretrained=True).features.eval()
+
+cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
+cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
+
+class Normalization(nn.Module):
+    def __init__(self, mean, std):
+        super(Normalization, self).__init__()
+        self.mean = torch.as_tensor(mean).view(-1, 1, 1)
+        self.std = torch.as_tensor(std).view(-1, 1, 1)
+
+    def forward(self, img):
+        
+        # Normalize image
+        return (img - self.mean) / self.std
+
 
 # desired depth layers to compute style/content losses :
+content_layers_default = ['conv_5']
+style_layers_default = ['conv_1', 'conv_3', 'conv_5', 'conv_9', 'conv_13']
 
-# content layer: ‘conv3 1’ with index 5
-content_layers_idx = [5]
+def get_style_model_and_losses(cnn, normalization_mean, normalization_std,
+                               style_img, content_img,
+                               content_layers=content_layers_default,
+                               style_layers=style_layers_default):
+    cnn = copy.deepcopy(cnn)
 
-'''
-conv1_1 with index 1
-conv2_1 with index 3
-conv3_1 with index 5
-conv4_1 with index 9
-conv5_1 with index 13
-'''
+    # Normalization module
+    normalization = Normalization(normalization_mean, normalization_std).to(device)
 
-style_layers_idx = [1, 3, 5, 9, 13]
-
-def get_style_model_and_losses(net, style_img, content_img, 
-                               content_layers=content_layers_idx, 
-                               style_layers=style_layers_idx):
-    
-    vgg = copy.deepcopy(net)
-    
     # losses
     content_losses = []
     style_losses = []
-    
-    model = nn.Sequential()
-    
+
+    model = nn.Sequential(normalization)
+
     i = 0
-    for layer in vgg.children():
-        
+    for layer in cnn.children():
         if isinstance(layer, nn.Conv2d):
             i += 1
             name = 'conv_{}'.format(i)
@@ -133,34 +119,82 @@ def get_style_model_and_losses(net, style_img, content_img,
             name = 'bn_{}'.format(i)
         else:
             raise RuntimeError('Unrecognized layer: {}'.format(layer.__class__.__name__))
-        
+
         model.add_module(name, layer)
-        
-        if i in content_layers:
+
+        if name in content_layers:
             # add content loss:
             target = model(content_img).detach()
             content_loss = ContentLoss(target)
             model.add_module("content_loss_{}".format(i), content_loss)
             content_losses.append(content_loss)
 
-        if i in style_layers:
+        if name in style_layers:
             # add style loss:
             target_feature = model(style_img).detach()
             style_loss = StyleLoss(target_feature)
             model.add_module("style_loss_{}".format(i), style_loss)
             style_losses.append(style_loss)
-    
-    
+
     # Trim off the layers after the last content and style losses
     for i in range(len(model) - 1, -1, -1):
         if isinstance(model[i], ContentLoss) or isinstance(model[i], StyleLoss):
             break
-    
+
     model = model[:(i + 1)]
-    
+
     return model, style_losses, content_losses
+
+
+def run_style_transfer(cnn, normalization_mean, normalization_std,
+                       content_img, style_img, input_img, num_steps=500,
+                       style_weight=100000, content_weight=1):
     
-    
-    
-    
-    
+    print('Building the style transfer model...')
+    model, style_losses, content_losses = get_style_model_and_losses(cnn,
+        normalization_mean, normalization_std, style_img, content_img)
+    optimizer = optim.LBFGS([input_img.requires_grad_()])
+
+    print('Optimizing...')
+    run = [0]
+    while run[0] <= num_steps:
+
+        def closure():
+            # correct the values of updated input image
+            input_img.data.clamp_(0, 1)
+
+            optimizer.zero_grad()
+            model(input_img)
+            style_score = 0
+            content_score = 0
+
+            for sl in style_losses:
+                style_score += sl.loss
+            for cl in content_losses:
+                content_score += cl.loss
+
+            style_score *= style_weight
+            content_score *= content_weight
+
+            loss = style_score + content_score
+            loss.backward()
+
+            run[0] += 1
+            if run[0] % 50 == 0:
+                print("run {}, Style Loss: {:4f}, Content Loss: {:4f}".format(
+                    run, style_score.item(), content_score.item()))
+
+            return style_score + content_score
+
+        optimizer.step(closure)
+
+    # Last correction
+    input_img.data.clamp_(0, 1)
+
+    return input_img
+
+
+
+
+
+
