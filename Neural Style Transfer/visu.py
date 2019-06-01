adb476e3482c8dcfb81180a306c189f127227e09
qiminchen <liam951120@gmail.com>
2019-05-31 23:52:06 -0700

    Update visu.py
    add convert function to convert tensor to numpy

--- Neural Style Transfer/visu.py
+++ Neural Style Transfer/visu.py
@@ -9,18 +9,21 @@ Created on Fri May 31 11:55:37 2019
 import matplotlib.pyplot as plt
 import numpy as np
 
+def convert(img):
+    
+    img = img.to('cpu').detach().numpy()
+    img = np.moveaxis(img, [0, 1, 2], [2, 0, 1])
+    img[img < 0] = 0
+    img[img > 1] = 1
+    
+    return img
+
 def display(img1, img2):
     
-    img1 = img1.to('cpu').detach().numpy()
-    img1 = np.moveaxis(img1, [0, 1, 2], [2, 0, 1])
-    img1[img1 < 0] = 0
-    img1[img1 > 1] = 1
-    img2 = img2.to('cpu').detach().numpy()
-    img2 = np.moveaxis(img2, [0, 1, 2], [2, 0, 1])
-    img2[img2 < 0] = 0
-    img2[img2 > 1] = 1
+    img1 = convert(img1)
+    img2 = convert(img2)
     
-    fig, ax = plt.subplots(1, 2, figsize=(6,3))
+    fig, ax = plt.subplots(1, 2, figsize=(9,6))
     ax[0].set_title('Style')
     ax[0].imshow(img1)
     ax[1].set_title('Content')
